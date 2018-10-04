db7d3e1ec50c716744bc438dba31ec4c7b6bc4eb
aparnaperwala <39587393+aparnaperwala@users.noreply.github.com>
2018-10-04 15:54:26 -0700

    updated json with connection string, updated database and created data Seed

---
+++ EventCatalogAPI/Data/CatalogSeed.cs
@@ -0,0 +1,86 @@
+﻿using EventCatalogAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Data
+{
+    public class CatalogSeed
+    {
+        public static async Task SeedAsync(CatalogContext context)
+        {
+            context.Database.Migrate();
+            if(!context.EventOccasions.Any())
+            {
+                context.EventOccasions.AddRange(GetPreconfiguredEventOccasions());
+                context.SaveChanges();
+            }
+
+            context.Database.Migrate();
+            if (!context.EventLocations.Any())
+            {
+                context.EventLocations.AddRange(GetPreconfiguredEventLocations());
+                context.SaveChanges();
+            }
+
+            context.Database.Migrate();
+            if (!context.EventCategories.Any())
+            {
+                context.EventCategories.AddRange(GetPreconfiguredEventCategories());
+                context.SaveChanges();
+            }
+
+            context.Database.Migrate();
+            if (!context.EventItems.Any())
+            {
+                context.EventItems.AddRange(GetPreconfiguredEventItems());
+                context.SaveChanges();
+            }
+        }
+
+private static IEnumerable<EventOccasion> GetPreconfiguredEventOccasions()
+        {
+            return new List<EventOccasion>()
+            {
+                new EventOccasion {Occasion="Halloween"},
+                new EventOccasion {Occasion="Christmas"},
+                new EventOccasion {Occasion="NewYear"}
+            };
+        }
+
+
+        private static IEnumerable<EventLocation> GetPreconfiguredEventLocations()
+        {
+            return new List<EventLocation>()
+            {
+                new EventLocation {Location="Bothell"},
+                new EventLocation {Location="Bellevue"},
+                new EventLocation {Location="Seattle"}
+            };
+        }
+
+        private static IEnumerable<EventCategory> GetPreconfiguredEventCategories()
+        {
+            return new List<EventCategory>()
+            {
+                new EventCategory {Category="indoors"},
+                new EventCategory {Category="outdoor"}
+               
+            };
+        }
+
+        private static IEnumerable<EventItem> GetPreconfiguredEventItems()
+        {
+            return new List<EventItem>()
+            {
+                new EventItem() { EventOccasionId=2,EventLocationId=3,EventCategoryId=3, Description = "Shoes for next century", Name = "World Star", Price = 199.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/1" },
+            };
+        }
+
+
+
+
+    }
+}
---
+++ EventCatalogAPI/Migrations/20181004195307_InitialMigration.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventCatalogAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "event_item_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "event_occasion_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "EventCategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Category = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventCategory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventLocation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Location = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventLocation", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventOcassion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Occasion = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventOcassion", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    PictureUrl = table.Column<string>(nullable: true),
+                    EventLocationId = table.Column<int>(nullable: false),
+                    EventOccasionId = table.Column<int>(nullable: false),
+                    EventCategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventItem_EventCategory_EventCategoryId",
+                        column: x => x.EventCategoryId,
+                        principalTable: "EventCategory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventItem_EventLocation_EventLocationId",
+                        column: x => x.EventLocationId,
+                        principalTable: "EventLocation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventItem_EventOcassion_EventOccasionId",
+                        column: x => x.EventOccasionId,
+                        principalTable: "EventOcassion",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventItem_EventCategoryId",
+                table: "EventItem",
+                column: "EventCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventItem_EventLocationId",
+                table: "EventItem",
+                column: "EventLocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventItem_EventOccasionId",
+                table: "EventItem",
+                column: "EventOccasionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventItem");
+
+            migrationBuilder.DropTable(
+                name: "EventCategory");
+
+            migrationBuilder.DropTable(
+                name: "EventLocation");
+
+            migrationBuilder.DropTable(
+                name: "EventOcassion");
+
+            migrationBuilder.DropSequence(
+                name: "event_item_hilo");
+
+            migrationBuilder.DropSequence(
+                name: "event_occasion_hilo");
+        }
+    }
+}
